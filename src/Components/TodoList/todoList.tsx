132a38e8e232546f2b1a9eb32cc5490414ce82cb
lemon <77824886+jaemin96@users.noreply.github.com>
2022-06-22 00:14:16 +0900

    Todo 기본 세팅

---
+++ src/Components/TodoList/todoList.tsx
@@ -0,0 +1,9 @@
+const TodoList = () => {
+  return (
+    <>
+      <h1>todo_list</h1>
+    </>
+  );
+};
+
+export default TodoList;
--- src/Page/Todo/todo.tsx
+++ src/Page/Todo/todo.tsx
@@ -1,10 +1,39 @@
+import React, { FormEvent, useEffect, useState } from "react";
+import TodoList from "../../Components/TodoList/todoList";
 import "./todo.scss";
 
+interface TodoType {
+  id: number;
+  title: string;
+  status: "pending" | "done";
+}
+
 const Todo = () => {
+  const [title, setTitle] = useState<string>("");
+  const [todoItem, setTodoItem] = useState<TodoType>();
+
+  const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setTitle("");
+  };
+
+  const handleTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  };
+
+  useEffect(() => {
+    console.log("title", title);
+  }, [title]);
+
   return (
-    <>
-      <h1>this page is TodoList</h1>
-    </>
+    <div>
+      <form onSubmit={handleOnSubmit}>
+        <input type="text" name="title" value={title} onChange={handleTitle} />
+        <button>add</button>
+      </form>
+      {/* 투두작성 */}
+      <TodoList />
+    </div>
   );
 };
 
