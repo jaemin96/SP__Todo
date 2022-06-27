03cbd365be7301c748fab13f0fa82f9945b588c4
lemon <77824886+jaemin96@users.noreply.github.com>
2022-06-27 23:46:22 +0900

    todo 리스트 화면에 표시
    + 리스트 화면에 표시되도록 map 함수 로직 타기
    - 리스트 항목별 key값 부여 & useState 타입지정 문제 발생 및 해결완료

--- src/Components/TodoList/todoList.tsx
+++ src/Components/TodoList/todoList.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { TodoType } from "../../Page/Todo/todo";
 
 type Todo = {
@@ -6,13 +6,23 @@ type Todo = {
 };
 
 const TodoList = ({ items }: Todo): React.ReactElement => {
+  const [todos, setTodos] = useState<TodoType[]>();
+
   useEffect(() => {
     console.log("items", items);
+    setTodos(items);
   }, [items]);
+
+  useEffect(() => {
+    console.log("todos", todos);
+  }, [todos]);
 
   return (
     <>
-      <h1>todo_list</h1>
+      {todos &&
+        todos.map((item: TodoType, index: number) => {
+          return <h1 key={item.id}>{item.title}</h1>;
+        })}
     </>
   );
 };
